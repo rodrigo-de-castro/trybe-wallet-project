e845664bad261c32e88271ba88c884a93933ee25
Rodrigo de Castro <rodrigo.cs.dev@gmail.com>
2022-04-01 16:45:53 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,19 +10,77 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { emailUser } = this.props;
+    const { emailUser, currencies } = this.props;
     return (
-      <header>
-        <p data-testid="email-field">{emailUser}</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <>
+        <header>
+          <p data-testid="email-field">{emailUser}</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+        <form>
+          <label htmlFor="valueInput">
+            Valor:
+            <input
+              id="valueInput"
+              data-testid="value-input"
+              name="valueInput"
+            />
+          </label>
+          <label htmlFor="descriptionInput">
+            Descrição:
+            <input
+              id="descriptionInput"
+              data-testid="description-input"
+              name="descriptionInput"
+            />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select
+              id="moeda"
+              name="moeda"
+              label="moeda"
+            >
+              {currencies
+                .map((currency) => (<option key={ currency }>{currency}</option>))}
+            </select>
+          </label>
+          <label htmlFor="methodInput">
+            Método de Pagamento:
+            <select
+              id="methodInput"
+              data-testid="method-input"
+              name="methodInput"
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tagInput">
+            Categoria:
+            <select
+              id="tagInput"
+              data-testid="tag-input"
+              name="tagInput"
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   emailUser: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -31,6 +89,7 @@ const mapDispatchToProps = (dispatch) => ({
 Wallet.propTypes = {
   emailUser: PropTypes.objectOf(PropTypes.string).isRequired,
   fetchCurrency: PropTypes.func.isRequired,
+  currencies: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
